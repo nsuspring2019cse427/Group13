68b82d5a31fb03bc5ad47705468d5cd7960a75bf
MajedurRahman <Majedur.rahman.mmr@gmail.com>
2019-02-23 22:46:23 +0600

    Added CORS Config

--- Project01/Server/src/main/java/com/cse427/server/ServerApplication.java
+++ Project01/Server/src/main/java/com/cse427/server/ServerApplication.java
@@ -2,6 +2,9 @@ package com.cse427.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -11,4 +14,3 @@ public class ServerApplication {
     }
 
 }
-
