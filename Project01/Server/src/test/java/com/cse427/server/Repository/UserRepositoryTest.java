260a1be67658c7138ee578a803350190fbad1b01
Md majedur rahman <majedur.rahman.mmr@gmail.com>
2019-03-20 22:21:43 +0600

    Initial getUserByActiveIsTrue Test Signature

--- Project01/Server/src/test/java/com/cse427/server/Repository/UserRepositoryTest.java
+++ Project01/Server/src/test/java/com/cse427/server/Repository/UserRepositoryTest.java
@@ -48,11 +48,21 @@ public class UserRepositoryTest {
         User user = new User(eamil, nameUser, pass, isActive);
         System.out.println(user.toString());
         given(userRepository.getUserByUserNameAndActive(nameUser, isActive)).willReturn(Optional.of(user));
-
-
         Optional<User> userByUserNameAndActive = userRepository.getUserByUserNameAndActive(nameUser, isActive);
-
         assertEquals(userByUserNameAndActive, Optional.of(user));
+
+    }
+
+
+    @Test
+    @Parameters(value = {
+            "MajedurRahman , true , Majedur.Rahman.mmr@gmail.com , asd89sa9d8s",
+            "Monit , true , mmr@gmail.com , smdsnmd",
+    })
+    public void getUserByActiveIsTrue(String nameUser, boolean isActive, String eamil, String pass) {
+        User user = new User(eamil, nameUser, pass, isActive);
+        System.out.println(user.toString());
+
 
     }
 }
