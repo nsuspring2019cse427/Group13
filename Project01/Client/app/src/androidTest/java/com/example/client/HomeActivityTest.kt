fda6ef9bb74638d5ce3eb9dcaa1c8595c462322e
Md majedur rahman <majedur.rahman.mmr@gmail.com>
2019-04-21 12:59:41 +0600

    add ui test for Home activity.
    Initial views and perform click

--- Project01/Client/app/src/androidTest/java/com/example/client/HomeActivityTest.kt
+++ Project01/Client/app/src/androidTest/java/com/example/client/HomeActivityTest.kt
@@ -1,29 +1,55 @@
 package com.example.client
 
-import android.app.Instrumentation
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
-
-import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-
+import kotlin.concurrent.thread
 
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class HomeActivityTest {
 
-    @Before
-    fun setUp() {
+
+
+    @Rule
+    @JvmField
+    val rule = ActivityTestRule(HomeActivity::class.java)
+
+
+    @Test
+    fun isInitialComponentVisibleTest() {
+
+        onView(withId(R.id.loginButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.signUpButton)).check(matches(isDisplayed()))
+
     }
 
-    @After
-    fun tearDown() {
+
+
+    @Test
+    fun loginButtonClickTest(){
+
+        onView(withId(R.id.loginButton)).check(matches(isClickable()))
+        onView(withId(R.id.loginButton)).perform(click())
     }
 
+
+
+    @Test
+    fun signUpButtonClickTest(){
+        onView(withId(R.id.loginButton)).check(matches(isClickable()))
+        onView(withId(R.id.signUpButton)).perform(click())
+    }
 
 
 }
--- Project01/Client/app/src/androidTest/java/com/example/client/LoginActivityTest.kt
+++ Project01/Client/app/src/androidTest/java/com/example/client/LoginActivityTest.kt
@@ -3,20 +3,20 @@ package com.example.client
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.BoundedMatcher
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.text.method.PasswordTransformationMethod
+import android.view.View
 import android.widget.EditText
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import android.text.method.PasswordTransformationMethod
-import android.support.test.espresso.matcher.BoundedMatcher
-import android.view.View
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 
 
 @RunWith(AndroidJUnit4::class)
@@ -31,18 +31,8 @@ class LoginActivityTest {
     val password: String = "secretPass"
 
 
-    @Before
-    fun setUp() {
-    }
-
-
-    @After
-    fun tearDown() {
-    }
-
-
     @Test
-    fun isInitialComponentVisible() {
+    fun isInitialComponentVisibleTest() {
         onView(withId(R.id.emailEdittext)).check(matches(isDisplayed()))
         onView(withId(R.id.passwordEdittext)).check(matches(isDisplayed()))
         onView(withId(R.id.signInButton)).check(matches(isDisplayed()))
@@ -54,11 +44,10 @@ class LoginActivityTest {
 
 
         onView(withId(R.id.emailEdittext)).check(matches(isFocusable()))
-
         onView(withId(R.id.emailEdittext)).perform(typeText(email), closeSoftKeyboard())
 
         onView(withId(R.id.passwordEdittext)).perform(typeText(password), closeSoftKeyboard())
-        onView(withId(R.id.passwordEdittext)).check(matches(isPasswordHidden()));
+        onView(withId(R.id.passwordEdittext)).check(matches(isPasswordHidden()))
 
 
         onView(withId(R.id.signInButton)).perform(click())
